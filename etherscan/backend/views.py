8991ff3a1712ed52bff81af6c5b1dfa2e292c6f9
Kimberly <kimberly19970123@gmail.com>
2022-11-09 21:54:01 -0500

    add an example endpoint

--- etherscan/backend/views.py
+++ etherscan/backend/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+
+@csrf_exempt
+@api_view(["GET"])
+def ercprotocols(request):
+    """ Get all NFT protocol (ENUM) 
+
+    :method GET
+    :param request:
+    :return:
+        :: status = 200 (OK)
+        :: result: [{value: ,name: }]
+    """
+    context = {}
+    if request.method == "GET":
+
+        return Response(context, status=status.HTTP_200_OK)
